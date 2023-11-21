32e9dd92c8d5e9ccf5c9022fdb1cff15be790f93
H4kt <h4ktoff@gmail.com>
2023-11-21 15:40:17 +0300

    init CI
    Took 27 minutes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,12 +1,18 @@
 plugins {
+
     kotlin("jvm") version "1.9.20"
     kotlin("plugin.serialization") version "1.9.20"
+
+    id("maven-publish")
+    id("co.uzzu.dotenv.gradle") version "2.0.0"
+
 }
 
 group = "dev.h4kt"
 version = "1.1.0"
 
 val ktorVersion: String by project
+val gitlabProjectId: String by project
 
 repositories {
     mavenCentral()
@@ -33,10 +39,55 @@ dependencies {
 
 }
 
-tasks.test {
-    useJUnitPlatform()
-}
-
 kotlin {
     jvmToolchain(11)
+}
+
+java {
+    withSourcesJar()
+    withJavadocJar()
+}
+
+publishing {
+
+    repositories {
+
+        maven {
+            name = "GitHubPackageRegistry"
+            url = uri("https://maven.pkg.github.com/H4kt/ktor-docs")
+            credentials {
+                username = System.getenv("GITHUB_ACTOR")
+                password = System.getenv("GITHUB_TOKEN")
+            }
+        }
+
+        maven {
+
+            name = "IOTechGitLab"
+            url = uri("https://git.iotech.company/api/v4/projects/$gitlabProjectId/packages/maven")
+
+            credentials(HttpHeaderCredentials::class) {
+                name = "Private-Token"
+                value = System.getenv("GITLAB_ACCESS_TOKEN")
+            }
+
+            authentication {
+                create<HttpHeaderAuthentication>("header")
+            }
+
+
+        }
+
+    }
+
+    publications {
+        register("ktorDocs", MavenPublication::class.java) {
+            from(components["java"])
+        }
+    }
+
+}
+
+tasks.test {
+    useJUnitPlatform()
 }
