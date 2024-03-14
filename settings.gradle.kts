d5129e17bfbd6311224628713628c10e7a43fe71
H4kt <h4ktoff@gmail.com>
2024-03-15 02:25:04 +0300

    codestyle: added commit linting via gradle plugin
    Took 8 minutes

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -7,6 +7,19 @@ pluginManagement {
 
 plugins {
     id("org.gradle.toolchains.foojay-resolver-convention") version "0.5.0"
+    id("org.danilopianini.gradle-pre-commit-git-hooks") version "2.0.1"
 }
 
 rootProject.name = "ktor-docs"
+
+gitHooks {
+
+    commitMsg {
+        conventionalCommits {
+            types("feature", "refactoring", "fix", "codestyle", "docs", "revert", "ci", "chore")
+        }
+    }
+
+    createHooks(overwriteExisting = true)
+
+}
